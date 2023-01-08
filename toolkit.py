40098752359f72473a721ad5dea32f7266a187da
JIAN-MING TU <43849007+vony123tony123@users.noreply.github.com>
2023-01-09 00:44:54 +0800

    add recover_filter_from_result
    add recover_filter_from_result function

--- toolkit.py
+++ toolkit.py
@@ -8,3 +8,12 @@ def two_time_convolution(filter_a, filter_b):
 			tmp = filter_a * filter_b[y_b][x_b]
 			result[y_b:y_b+len(filter_a), x_b:x_b+len(filter_a)] += tmp
 	return result
+
+def recover_filter_from_result(result, filter_b):
+	size = len(result) - len(filter_b) + 1
+	filter_a = np.zeros((size, size), dtype=np.result_type(result))
+	for y in range(len(filter_a)):
+		for x in range(len(filter_a[0])):
+			filter_a[y][x] = result[y][x]
+			result[y:y+len(filter_b), x:x+len(filter_b)] -= filter_a[y][x] * filter_b
+	return filter_a
